ebc6907321d835ddbcb63bac3ed577fb33b76f06
Akanksha Dey <126316084+Akanksha-Dey@users.noreply.github.com>
2024-03-20 08:45:29 +0530

    Update employee.cpp

--- employee.cpp
+++ employee.cpp
@@ -1,217 +1,209 @@
+
+
+
 #include <iostream>
-#include <conio.h>
+#include <string>  // Include the <string> header for using std::string
+#include <cstdlib> // Include the <cstdlib> header for system function
 using namespace std;
-int i=0;
+
+int i = 0;
 struct Employee
 {
     int id;
     float salary;
-    string name,address,phone,department;
+    string name, address, phone, department;
+} E[25];
 
-}E[25];
-void insert()  //insert new employee record
+void insert()
 {
     system("cls");
-    cout<<"\n\n Insert Employee Record";
-    cout<<"\n\n Enter Employee ID:";
-    cin>>E[i].id;
-    cout<<"Enter Employee Name:";
+    cout << "\n\n Insert Employee Record";
+    cout << "\n\n Enter Employee ID:";
+    cin >> E[i].id;
+    cout << "Enter Employee Name:";
     cin.ignore();
-    getline(cin,E[i].name);
-    cout<<"Enter Employee Address:";
-    getline(cin,E[i].address);
-    cout<<"Enter Employee Phone:";
-    cin>>E[i].phone;
-    cout<<"Enter Employee Department:";
+    getline(cin, E[i].name);
+    cout << "Enter Employee Address:";
+    getline(cin, E[i].address);
+    cout << "Enter Employee Phone:";
+    cin >> E[i].phone;
+    cout << "Enter Employee Department:";
     cin.ignore();
-    getline(cin,E[i].department);
-    cout<<"Enter Employee Salary:";
-    cin>>E[i].salary;
+    getline(cin, E[i].department);
+    cout << "Enter Employee Salary:";
+    cin >> E[i].salary;
     i++;
-    cout<<"\n\n Employee Record Inserted";
-
+    cout << "\n\n Employee Record Inserted";
 }
+
 void search()
 {
     system("cls");
-    cout<<"\n\n EMployee Management System";
-    if(i==0)
+    cout << "\n\n Employee Management System";
+    if (i == 0)
     {
-        cout<<"\n\n Data Base Is Empty";
-    
+        cout << "\n\n Database Is Empty";
     }
     else
     {
-        int t_id,found=0;;
-        cout<<"\n\n Enter Employee ID For Search:";
-        cin>>t_id;
-        for(int a=0;a<i;a++)
+        int t_id, found = 0;
+        cout << "\n\n Enter Employee ID For Search:";
+        cin >> t_id;
+        for (int a = 0; a < i; a++)
         {
-            if(t_id==E[a].id)
+            if (t_id == E[a].id)
             {
-                cout<<"\n\n Employee Name:"<<E[a].name;
-                cout<<"\n\n Employee Address:"<<E[a].address;
-                cout<<"\n\n Employee Phone No.:"<<E[a].phone;
-                cout<<"\n\n Employee Department:"<<E[a].department;
-                cout<<"\n\n Employee Salary:"<<E[a].salary;
+                cout << "\n\n Employee Name:" << E[a].name;
+                cout << "\n\n Employee Address:" << E[a].address;
+                cout << "\n\n Employee Phone No.:" << E[a].phone;
+                cout << "\n\n Employee Department:" << E[a].department;
+                cout << "\n\n Employee Salary:" << E[a].salary;
                 found++;
             }
         }
-        if(found==0)
+        if (found == 0)
         {
-            cout<<"\n\n Employee ID Not Found";
+            cout << "\n\n Employee ID Not Found";
         }
     }
 }
+
 void update()
 {
     system("cls");
-    cout<<"\n\n EMployee Management System";
-    if(i==0)
+    cout << "\n\n Employee Management System";
+    if (i == 0)
     {
-        cout<<"\n\n Data Base Is Empty";
-    
+        cout << "\n\n Database Is Empty";
     }
     else
     {
-        int t_id,found;;
-        cout<<"\n\n Enter Employee ID For Update:";
-        cin>>t_id;
-        for(int a=0;a<i;a++)
+        int t_id, found = 0;
+        cout << "\n\n Enter Employee ID For Update:";
+        cin >> t_id;
+        for (int a = 0; a < i; a++)
         {
-            if(t_id==E[a].id)
+            if (t_id == E[a].id)
             {
-                cout<<"\n\n Enter Employee Name:";
+                cout << "\n\n Enter Employee Name:";
                 cin.ignore();
-                getline(cin,E[a].name);
-                cout<<"\n\n Enter Employee Address:";
-                getline(cin,E[a].address);
-                cout<<"\n\n Enter Employee Phone No.:";
-                cin>>E[a].phone;
-                cout<<"\n\n Enter Employee Department:";
+                getline(cin, E[a].name);
+                cout << "\n\n Enter Employee Address:";
+                getline(cin, E[a].address);
+                cout << "\n\n Enter Employee Phone No.:";
+                cin >> E[a].phone;
+                cout << "\n\n Enter Employee Department:";
                 cin.ignore();
-                getline(cin,E[a].department);
-                cout<<"\n\n Enter Employee Salary:";
-                cin>>E[a].salary;
-                cout<<"//Record Upated Successfully//";
+                getline(cin, E[a].department);
+                cout << "\n\n Enter Employee Salary:";
+                cin >> E[a].salary;
+                cout << "\n\n Record Updated Successfully";
                 found++;
             }
         }
-        if(found==0)
+        if (found == 0)
         {
-            cout<<"\n\n Employee ID Not Found";
+            cout << "\n\n Employee ID Not Found";
         }
     }
 }
+
 void del()
 {
     system("cls");
-    cout<<"\n\n EMployee Management System";
-    if(i==0)
+    cout << "\n\n Employee Management System";
+    if (i == 0)
     {
-        cout<<"\n\n Data Base Is Empty";
-    
+        cout << "\n\n Database Is Empty";
     }
     else
     {
-        int t_id,found;;
-        cout<<"\n\n Enter Employee ID For Delete:";
-        cin>>t_id;
-        for(int a=0;a<i;a++)
+        int t_id, found = 0;
+        cout << "\n\n Enter Employee ID For Delete:";
+        cin >> t_id;
+        for (int a = 0; a < i; a++)
         {
-            if(t_id==E[a].id)
+            if (t_id == E[a].id)
             {
-                for(int k=a;k<i-1;k++)
+                for (int k = a; k < i - 1; k++)
                 {
-                    E[k].id=E[k].id;
-                    E[k].name=E[k].name;
-                    E[k].address=E[k].address;
-                    E[k].phone=E[k].phone;
-                    E[k].department=E[k].department;
-                    E[k].salary=E[k].salary;
-                    
+                    E[k] = E[k + 1]; // Shift elements to overwrite the deleted record
                 }
                 i--;
-                cout<<"//Record Deleted Successfully//";
+                cout << "\n\n Record Deleted Successfully";
                 found++;
                 break;
             }
         }
-        if(found==0)
+        if (found == 0)
         {
-            cout<<"\n\n Employee ID Not Found";
+            cout << "\n\n Employee ID Not Found";
         }
     }
 }
+
 void show()
 {
     system("cls");
-    cout<<"\n\n EMployee Management System";
-    if(i==0)
+    cout << "\n\n Employee Management System";
+    if (i == 0)
     {
-        cout<<"\n\n Data Base Is Empty";
-    
+        cout << "\n\n Database Is Empty";
     }
     else
     {
-        
-        for(int a=0;a<i;a++)
+        for (int a = 0; a < i; a++)
         {
-                cout<<"\n\n Employee ID:"<<E[a].id;
-                cout<<"\n\n Employee Name:"<<E[a].name;
-                cout<<"\n\n Employee Address:"<<E[a].address;
-                cout<<"\n\n Employee Phone No.:"<<E[a].phone;
-                cout<<"\n\n Employee Department:"<<E[a].department;
-                cout<<"\n\n Employee Salary:"<<E[a].salary;
+            cout << "\n\n Employee ID:" << E[a].id;
+            cout << "\n\n Employee Name:" << E[a].name;
+            cout << "\n\n Employee Address:" << E[a].address;
+            cout << "\n\n Employee Phone No.:" << E[a].phone;
+            cout << "\n\n Employee Department:" << E[a].department;
+            cout << "\n\n Employee Salary:" << E[a].salary;
         }
-       
     }
 }
- main()
-{
-    {
-    p:
-    system("cls");
-    int choice;
-    cout<<"\n\n Employee Management System";
-    cout<<"\n\n 1. Insert Employee";
-    cout<<"\n\n 2. Search Employee";
-    cout<<"\n\n 3. Update Employee";
-    cout<<"\n\n 4. Delete Employee";
-    cout<<"\n\n 5. Show All Employee";
-    cout<<"\n\n 6. Exit";
-    cout<<"\n\n  Enter Your Choice:";
-    cin>> choice;
-   
-    switch(choice)
-    {
-        case 1:
-             insert();
 
-             break;
-        case 2:
-             search();
-             break;
-        case 3:
-             update();
-             break;
-        case 4:
-             del();
-             break;
-        case 5:
-             show();
-             break;
-        case 6:
-             exit(0);
-        default:
-            cout<<"\n\n Invalid Option.... Please Try Again";
+int main() // Use int main() instead of main()
+{
+    int choice;
+p:
+    system("cls");
+    cout << "\n\n Employee Management System";
+    cout << "\n\n 1. Insert Employee";
+    cout << "\n\n 2. Search Employee";
+    cout << "\n\n 3. Update Employee";
+    cout << "\n\n 4. Delete Employee";
+    cout << "\n\n 5. Show All Employee";
+    cout << "\n\n 6. Exit";
+    cout << "\n\n Enter Your Choice:";
+    cin >> choice;
+
+    switch (choice)
+    {
+    case 1:
+        insert();
+        break;
+    case 2:
+        search();
+        break;
+    case 3:
+        update();
+        break;
+    case 4:
+        del();
+        break;
+    case 5:
+        show();
+        break;
+    case 6:
+        exit(0);
+    default:
+        cout << "\n\n Invalid Option.... Please Try Again";
     }
-     getchar ();
+    cin.ignore();
+    cin.get();
     goto p;
 }
-}
-
-         
-
 
 
